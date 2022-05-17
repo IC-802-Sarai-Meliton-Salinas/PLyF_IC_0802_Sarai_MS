98b8fdd33fa7ad9bf04d0ef457c519c83529ef11
IC-802-Sarai-Meliton-Salinas <104597274+IC-802-Sarai-Meliton-Salinas@users.noreply.github.com>
2022-05-17 08:14:27 -0500

    Add files via upload

---
+++ Examen Practico/1 Prueba Examen/prueba.java
@@ -0,0 +1,17 @@
+Public interface prueba examen{
+System.out.println("hola");
+System.out.println("hola mi nombre es", "sarai");
+}
+
+Public String prueba examen{
+public String ("hola");
+System.out.println("  ");
+}
+Public Static operacion prueba int {
+public Static ("int5", "int2");
+operacion prueba(int + int2);
+}
+Public Abstract operacion prueba{
+public Abstract("int5", "int2", "int7");
+}
+}
---
+++ Examen Practico/2 Class Principal/Principal.java
@@ -0,0 +1,36 @@
+Public class principal {
+    public class Args []
+
+  main.prueba examen.pe;
+  System.out.println ==> (int2, int1, int4);
+  System.out.println ==> (int2, + int1, + int4);
+  
+  main.prueba.pe;
+  System.out.println ("int10, int6");
+  System.out.println ("int10, + int6");
+  System.out.println ("hola");
+  System.out.println ("hola","did");
+  pe.println (int10)+(int6)*(int4);
+  }
+  pe.operacion prueba Args [10]{
+  Args.operacion prueba [10], [6];
+  Args.operacion prueba [11], [2];
+}
+
+mi metodo.prueba.Args[]{
+Static void.prueba[11];
+Static void.prueba[11], [12];
+Static void.prueba[11], [12], [8];
+}
+main.prueba.pe{
+mi metodo.pe();
+System.out.println ("10.3"), ("6.33"), ("2.25");
+mi metodo.pe;
+pe.println("int6"), ("int4"), ("int8");
+}
+}
+
+
+  
+  
+  
